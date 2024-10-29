81af4ecea5dcb9ed8118356875a75f6e5e984bdb
MukundSaiRathod <s572099@nwmissouri.edu>
2024-10-29 01:03:36 -0500

    updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mydataRouter = require('./routes/mydata');
+var indexRouter = require('./index/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -15,7 +15,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use('/mydata', mydataRouter);
+// app.use('/mydata', mydataRouter);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
