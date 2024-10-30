675bd3608eaac834e77c0b854dddddc6903692d9
MukundSaiRathod <s572099@nwmissouri.edu>
2024-10-29 22:53:43 -0500

    updated

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var value = 0;
+var total = 1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  value += 2;
+  // Add value to total
+  total += value;
+  // res.send('respond with a resource');
+  res.send(`Total is: ${total}`);
 });
 
 module.exports = router;
