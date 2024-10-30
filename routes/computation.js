9286d44e6020b017e7a4a50bf7cabb7391285dca
MukundSaiRathod <s572099@nwmissouri.edu>
2024-10-30 12:05:14 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,15 +1,32 @@
 var express = require('express'); 
 var router = express.Router();
 
+const mathFunctions = {
+  acosh: Math.acosh,
+  log1p: Math.log1p,
+  tan: Math.tan,
+  sqrt: Math.sqrt,
+};
 
-const selectedFunction = Math.sqrt; 
+router.get('/', function(req, res) {
 
-router.get('/', function(req, res, next) {
-  
-  const x = req.query.x ? parseFloat(req.query.x) : Math.random(); 
-  const result = selectedFunction(x);
+  const x = req.query.x ? parseFloat(req.query.x) : Math.random(); // Use random if no query provided
 
-  res.send(`${selectedFunction.name} applied to ${x} is ${result}`);
+  const results = {
+    acosh: x >= 1 ? mathFunctions.acosh(x) : 'undefined (x must be greater than 1)',
+    log1p: mathFunctions.log1p(x),
+    tan: mathFunctions.tan(x),
+    sqrt: mathFunctions.sqrt(x),
+  };
+
+  const responseString = `
+    acosh applied to ${x} is ${results.acosh} <br> <br>
+    log1p applied to ${x} is ${results.log1p} <br> <br>
+    tan applied to ${x} is ${results.tan}     <br> <br>
+    sqrt applied to ${x} is ${results.sqrt}    <br> <br>
+  `;
+
+  res.send(responseString.trim());
 });
 
 module.exports = router;
